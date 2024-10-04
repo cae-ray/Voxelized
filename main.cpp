145ab9428107a40fbbd3121ab59b64515f88aea6
Cae-PSQ <>
2024-10-04 14:57:28 +0200

    changed main to work with the octree manager

--- main.cpp
+++ main.cpp
@@ -6,20 +6,8 @@
 int main() {
     cSparseVoxelOctreeManager svoManager;
 
-    int x = 0;
-    int y = 0;
-    int z = 0;
-
-    svoManager.CreateOctree(x, y);
-    svoManager.SetVoxelCall(x, y, z);
-
-	//svo.SetAllVoxels();
-
-    // Debug
-    //std::cout << "Voxel (3, 2, 1) set: "    << svo.IsVoxelSet( 3,  2,  1) << "\n";
-    //std::cout << "Voxel (7, 7, 7) set: "    << svo.IsVoxelSet( 7,  7,  7) << "\n";
-    //std::cout << "Voxel (15, 15, 15) set: " << svo.IsVoxelSet(15, 15, 15) << "\n";
-    //std::cout << "Voxel (15, 15, 15) set: " << svo.IsVoxelSet(15, 15, 15) << "\n";
+    svoManager.CreateOctree(0, 0);
+    svoManager.SetVoxelCall(0, 0, 0);
 
     return 0;
 }
