b713766b92e4518fdcb357fcce58c812e09651a3
Cae-PSQ <cae.enby@gmail.com>
2024-10-06 18:49:20 +0200

    added the use of Octree and Manager namespaces under SVO

--- cSparseVoxelOctree.cpp
+++ cSparseVoxelOctree.cpp
@@ -1,73 +1,68 @@
 #include "cSparseVoxelOctree.h"
 
-// Octree node
-////////////////////////////////////////////////////////////////
-sOctreeNode::sOctreeNode() {
-    for (int i = 0; i < nodeChildren; ++i) {
-        mChildren[i] = nullptr;
-    }
-}
+namespace SVO {
+    namespace Octree {
+        sOctreeNode::sOctreeNode() {
+            for (int i = 0; i < nodeChildren; ++i) {
+                mChildren[i] = nullptr;
+            }
+        }
 
-sOctreeNode::~sOctreeNode() {
-    for (int i = 0; i < nodeChildren; ++i) {
-        delete mChildren[i];
-    }
-}
-////////////////////////////////////////////////////////////////
-// Octree node
-
-// Sparse voxel octree
-////////////////////////////////////////////////////////////////
-cSparseVoxelOctree::cSparseVoxelOctree(const int& octreeLocalX, const int& octreeLocalY) {
-    mRoot = new sOctreeNode();
-    mOctreeLocalX = octreeLocalX;
-    mOctreeLocalY = octreeLocalY;
-}
-
-void cSparseVoxelOctree::SetAllVoxels() {
-    for (int x = 0; x < octreeSize; ++x) {
-        for (int y = 0; y < octreeSize; ++y) {
-            for (int z = 0; z < octreeSize; ++z) {
-                SetVoxel(x, y, z);
+        sOctreeNode::~sOctreeNode() {
+            for (int i = 0; i < nodeChildren; ++i) {
+                delete mChildren[i];
             }
         }
     }
-}
-
-void cSparseVoxelOctree::SetVoxelRecursive(sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
-    if (depth == maxDepth) {
-        return; // We reached the max depth, don't subdivide further
+    SVO::Octree::cSparseVoxelOctree::cSparseVoxelOctree(const int& octreeLocalX, const int& octreeLocalY) {
+        mRoot = new sOctreeNode();
+        mOctreeLocalX = octreeLocalX;
+        mOctreeLocalY = octreeLocalY;
     }
 
-    int index = GetChildIndex(voxelWorldX, voxelWorldY, voxelWorldZ, depth);
-    if (!node->mChildren[index]) {
-        node->mChildren[index] = new sOctreeNode();
-        node->mVoxelMask.Set(index);    // Mark the child as active
+    void SVO::Octree::cSparseVoxelOctree::SetAllVoxels() {
+        for (int x = 0; x < octreeSize; ++x) {
+            for (int y = 0; y < octreeSize; ++y) {
+                for (int z = 0; z < octreeSize; ++z) {
+                    SetVoxel(x, y, z);
+                }
+            }
+        }
     }
 
-    SetVoxelRecursive(node->mChildren[index], voxelWorldX, voxelWorldY, voxelWorldZ, depth + 1);
-}
+    void SVO::Octree::cSparseVoxelOctree::SetVoxelRecursive(sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
+        if (depth == maxDepth) {
+            return; // We reached the max depth, don't subdivide further
+        }
 
-bool cSparseVoxelOctree::IsVoxelSetRecursive(const sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
-    if (depth == maxDepth) {
-        return true;    // We've reached the max depth, so the voxel is set
+        int index = GetChildIndex(voxelWorldX, voxelWorldY, voxelWorldZ, depth);
+        if (!node->mChildren[index]) {
+            node->mChildren[index] = new sOctreeNode();
+            node->mVoxelMask.Set(index);    // Mark the child as active
+        }
+
+        SetVoxelRecursive(node->mChildren[index], voxelWorldX, voxelWorldY, voxelWorldZ, depth + 1);
     }
 
-    int index = GetChildIndex(voxelWorldX, voxelWorldY, voxelWorldZ, depth);
-    if (!node->mChildren[index]) {
-        return false;   // No child at this index, voxel is not set
+    bool SVO::Octree::cSparseVoxelOctree::IsVoxelSetRecursive(const sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
+        if (depth == maxDepth) {
+            return true;    // We've reached the max depth, so the voxel is set
+        }
+
+        int index = GetChildIndex(voxelWorldX, voxelWorldY, voxelWorldZ, depth);
+        if (!node->mChildren[index]) {
+            return false;   // No child at this index, voxel is not set
+        }
+
+        return IsVoxelSetRecursive(node->mChildren[index], voxelWorldX, voxelWorldY, voxelWorldZ, depth + 1);
     }
 
-    return IsVoxelSetRecursive(node->mChildren[index], voxelWorldX, voxelWorldY, voxelWorldZ, depth + 1);
-}
+    int SVO::Octree::cSparseVoxelOctree::GetChildIndex(const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
+        int shift = maxDepth - depth - 1;   // Shift for the current depth
+        int xi = (voxelWorldX >> shift) & 1;          // X bit for the current depth
+        int yi = (voxelWorldY >> shift) & 1;          // Y bit for the current depth
+        int zi = (voxelWorldZ >> shift) & 1;          // Z bit for the current depth
 
-int cSparseVoxelOctree::GetChildIndex(const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth) {
-    int shift = maxDepth - depth - 1;   // Shift for the current depth
-    int xi = (voxelWorldX >> shift) & 1;          // X bit for the current depth
-    int yi = (voxelWorldY >> shift) & 1;          // Y bit for the current depth
-    int zi = (voxelWorldZ >> shift) & 1;          // Z bit for the current depth
-
-    return (xi << 2) | (yi << 1) | zi;
+        return (xi << 2) | (yi << 1) | zi;
+    }
 }
-////////////////////////////////////////////////////////////////
-// Sparse voxel octree 
--- cSparseVoxelOctreeManager.h
+++ cSparseVoxelOctreeManager.h
@@ -4,17 +4,21 @@
 #include <unordered_map>
 #include <string>
 
-class cSparseVoxelOctreeManager {
-    std::unordered_map<std::string, cSparseVoxelOctree*> mUnorderedMap;
+namespace SVO {
+    namespace Manager {
+        class cSparseVoxelOctreeManager {
+            std::unordered_map<std::string, SVO::Octree::cSparseVoxelOctree*> mUnorderedMap;
 
-public:
-    void CreateOctree  (const int& octreeLocalX, const int& octreeLocalY);
-    void DeleteOctree  (const int& octreeLocalX, const int& octreeLocalY);
+        public:
+            void CreateOctree  (const int& octreeLocalX, const int& octreeLocalY);
+            void DeleteOctree  (const int& octreeLocalX, const int& octreeLocalY);
 
-    void SetAllVoxels  (const int& voxelWorldX,  const int& voxelWorldY);
-    void SetVoxelCall  (const int& voxelWorldX,  const int& voxelWorldY, const int& voxelWorldZ);
-    void IsVoxelSetCall(const int& voxelWorldX,  const int& voxelWorldY, const int& voxelWorldZ);
+            void SetAllVoxels  (const int& voxelWorldX,  const int& voxelWorldY);
+            void SetVoxelCall  (const int& voxelWorldX,  const int& voxelWorldY, const int& voxelWorldZ);
+            void IsVoxelSetCall(const int& voxelWorldX,  const int& voxelWorldY, const int& voxelWorldZ);
 
-private:
-    std::string GetOctreeKey(const int& octreeLocalX, const int& octreeLocalY);
-};
+        private:
+            std::string GetOctreeKey(const int& octreeLocalX, const int& octreeLocalY);
+        };
+    }
+}
