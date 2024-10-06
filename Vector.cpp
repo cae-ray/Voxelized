18f2ae83f57e675a32d214ce5634551de0f3ae1d
Cae-PSQ <cae.enby@gmail.com>
2024-10-06 19:21:02 +0200

    created a vector class

--- Vector.cpp
+++ Vector.cpp
@@ -1,5 +1,21 @@
 #include "Vector.h"
+#include <corecrt_math.h>
 
 namespace Math {
+	Vec3 Vec3::Normalize() const {
+		float magnitude = sqrt(x * x + y * y + z * z);
+		return { x / magnitude, y / magnitude, z / magnitude };
+	}
 
+	float Vec3::Dot(const Vec3& other) const {
+		return x * other.x + y * other.y + z * other.z;
+	}
+
+	Vec3 Vec3::Cross(const Vec3& other) const {
+		return {
+			y * other.z - z * other.y,
+			z * other.x - x * other.z,
+			x * other.y - y * other.x
+		};
+	}
 }
--- Vector.h
+++ Vector.h
@@ -1,7 +1,16 @@
 #pragma once
 
 namespace Math {
-	struct Vec3 {
+	class Vec3 {
+		Vec3 operator-(const Vec3& other) const {
+			return { x - other.x, y - other.y, z - other.z};
+		}
 
+	public:
+		float x, y, z;
+
+		Vec3  Normalize()				   const;
+		float Dot	   (const Vec3& other) const;
+		Vec3  Cross	   (const Vec3& other) const; 
 	};
 }
