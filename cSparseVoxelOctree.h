526a2ca4f194e1c08d7998a1e7898e3b80806804
Cae-PSQ <cae.enby@gmail.com>
2024-10-06 18:49:38 +0200

    Revert "added the use of Octree and Manager namespaces under SVO"
    This reverts commit b713766b92e4518fdcb357fcce58c812e09651a3.

--- cSparseVoxelOctree.h
+++ cSparseVoxelOctree.h
@@ -2,38 +2,43 @@
 #include "cBitset.h"
 #include "octreeSettings.h"
 
-namespace SVO {
-    namespace Octree {
-        struct sOctreeNode {
-            cBitSet      mVoxelMask;
-            sOctreeNode* mChildren[nodeChildren];
+// Octree node
+////////////////////////////////////////////////////////////////
+struct sOctreeNode {
+    cBitSet      mVoxelMask;
+    sOctreeNode* mChildren[nodeChildren];
 
-            // mChildren is initialized to null in constructor and deleted in destructor
-             sOctreeNode();
-            ~sOctreeNode();
+    // mChildren is initialized to null in constructor and deleted in destructor
+     sOctreeNode();
+    ~sOctreeNode();
 
-            // If no bits are set, it's a leaf node (no children)
-            bool IsLeaf() const { return mVoxelMask.None(); };
-        };
+    // If no bits are set, it's a leaf node (no children)
+    bool IsLeaf() const { return mVoxelMask.None(); };
+};
+////////////////////////////////////////////////////////////////
+// Octree node
 
-        class cSparseVoxelOctree {
-        public:
-            int mOctreeLocalX;
-            int mOctreeLocalY;
+// Sparse voxel octree
+////////////////////////////////////////////////////////////////
+class cSparseVoxelOctree
+{
+public:
+    int mOctreeLocalX;
+    int mOctreeLocalY;
 
-             cSparseVoxelOctree(const int& octreeLocalX, const int& octreeLocalY);
-            ~cSparseVoxelOctree() { delete mRoot; };
+     cSparseVoxelOctree(const int& octreeLocalX, const int& octreeLocalY);
+    ~cSparseVoxelOctree() { delete mRoot; };
     
-            void SetAllVoxels();
-            void SetVoxel    (const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ) { SetVoxelRecursive         (mRoot, voxelWorldX, voxelWorldY, voxelWorldZ, 0); };
-            bool IsVoxelSet  (const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ) { return IsVoxelSetRecursive(mRoot, voxelWorldX, voxelWorldY, voxelWorldZ, 0); };
+    void SetAllVoxels();
+    void SetVoxel    (const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ) { SetVoxelRecursive         (mRoot, voxelWorldX, voxelWorldY, voxelWorldZ, 0); };
+    bool IsVoxelSet  (const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ) { return IsVoxelSetRecursive(mRoot, voxelWorldX, voxelWorldY, voxelWorldZ, 0); };
 
-        private:
-            sOctreeNode* mRoot;
+private:
+    sOctreeNode* mRoot;
 
-            void SetVoxelRecursive  (      sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
-            bool IsVoxelSetRecursive(const sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
-            int  GetChildIndex      (                         const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
-        };
-    }
-}
+    void SetVoxelRecursive  (      sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
+    bool IsVoxelSetRecursive(const sOctreeNode* node, const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
+    int  GetChildIndex      (                         const int& voxelWorldX, const int& voxelWorldY, const int& voxelWorldZ, const int& depth);
+};
+////////////////////////////////////////////////////////////////
+// Sparse voxel octree 
