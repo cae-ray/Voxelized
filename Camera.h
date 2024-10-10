c96f68210a79f9fd86ef0e29df6a3de980a4c370
Cae-PSQ <cae.enby@gmail.com>
2024-10-10 21:36:34 +0200

    templated vectors

--- Camera.h
+++ Camera.h
@@ -1,4 +1,12 @@
 #pragma once
+#include "Vector.h"
+
 class Camera {
-	
+	Math::Vec3<float> cameraPosition  = Math::Vec3<float>(0.0f, 0.0f, 3.0f);
+	Math::Vec3<float> cameraTarget	  = Math::Vec3<float>(0.0f, 0.0f, 0.0f);
+	Math::Vec3<float> cameraDirection = Math::Vec3<float>(cameraPosition - cameraTarget);
+
+	Math::Vec3<float> up		  = Math::Vec3<float>(0.0f, 1.0f, 0.0f);
+	Math::Vec3<float> cameraRight = Math::Normalize(Math::Cross(up, cameraDirection));
+	Math::Vec3<float> cameraUp	  = Math::Cross(cameraDirection, cameraRight);
 };
--- Vector.h
+++ Vector.h
@@ -1,16 +1,37 @@
 #pragma once
+#include <corecrt_math.h>
 
 namespace Math {
+	template <class T>
 	class Vec3 {
+	public:
 		Vec3 operator-(const Vec3& other) const {
 			return { x - other.x, y - other.y, z - other.z};
 		}
 
-	public:
-		float x, y, z;
+		T x, y, z;
 
-		Vec3  Normalize()				   const;
-		float Dot	   (const Vec3& other) const;
-		Vec3  Cross	   (const Vec3& other) const; 
+		Vec3(T x, T y, T z) : x(x), y(y), z(z) {};
+
+	};
+
+	template <class T>
+	Vec3<T> Normalize(const Vec3<T>& vec) {
+		T magnitude = static_cast<T>(sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z));
+		return { vec.x / magnitude, vec.y / magnitude, vec.z / magnitude };
+	};
+
+	template < class T>
+	T Dot(const Vec3<T>& a, const Vec3<T>& b) {
+		return a.x * b.x + a.y * b.y + a.z * b.z;
+	};
+
+	template < class T>
+	Vec3<T> Cross(const Vec3<T>& a, const Vec3<T>& b) {
+		return {
+			a.y * b.z - a.z * b.y,
+			a.z * b.x - a.x * b.z,
+			a.x * b.y - a.y * b.x
+		};
 	};
 }
